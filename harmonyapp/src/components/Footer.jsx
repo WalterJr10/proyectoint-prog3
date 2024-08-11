b71cd6b63956333d07433c35a098275890656b12
Walter Agustín <101841775+WalterJr10@users.noreply.github.com>
2024-08-11 15:18:38 -0300

    footer-global

--- harmonyapp/src/components/Footer.jsx
+++ harmonyapp/src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
 
   return(
     <footer className="footer has-background-link-dark has-text-centered p-4">
-      <p className="has-text-warning is-size-5 is-uppercase has-text-weight-bold">Todos los derechos reservados - Walter Agustín Llanes {new Date().getFullYear()}</p>
+      <p className="has-text-warning is-size-5 is-uppercase has-text-weight-bold">Todos los derechos reservados - Grupo 18 Programacion {new Date().getFullYear()}</p>
     </footer>
   )
 
